eb4c2abcee9fae2905376b320f606410d04a93ce
feng.wei <feng.wei@gmail.com>
2021-09-21 23:11:32 +0800

    to antd pro v5

--- src/pages/chat/MsgUtils.ts
+++
@@ -1,7 +0,0 @@
-
-export function parseFileName(mediaUrl: string): string {
-    let fileSplits = mediaUrl.split('/') || [];
-    let s = fileSplits?.length || 0;
-    let fileName = fileSplits[s - 1];
-    return fileName;
-}
