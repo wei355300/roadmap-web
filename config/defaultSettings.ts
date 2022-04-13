65017d763f5763c4c7cc0bc630e0cef8294fabcf
feng.wei <feng.wei@gmail.com>
2022-04-13 14:47:49 +0800

    样式调整

--- config/defaultSettings.ts
+++ config/defaultSettings.ts
@@ -3,19 +3,34 @@ import {Settings as LayoutSettings} from '@ant-design/pro-layout';
 const Settings: LayoutSettings & {
   pwa?: boolean;
   logo?: string;
-} = {
-  navTheme: 'light',
-  // 拂晓蓝
-  primaryColor: '#1890ff',
-  layout: 'mix',
-  contentWidth: 'Fluid',
-  fixedHeader: false,
-  fixSiderbar: true,
-  colorWeak: false,
-  title: 'weido tapd-web',
-  pwa: false,
-  logo: 'https://gw.alipayobjects.com/zos/rmsportal/KDpgvguMpGfqaHPjicRK.svg',
-  iconfontUrl: '',
-};
+} =
+// {
+//   navTheme: 'light',
+//   // 拂晓蓝
+//   primaryColor: '#1890ff',
+//   layout: 'mix',
+//   contentWidth: 'Fluid',
+//   fixedHeader: false,
+//   fixSiderbar: true,
+//   colorWeak: false,
+//   title: 'weido tapd-web',
+//   pwa: false,
+//   logo: 'https://gw.alipayobjects.com/zos/rmsportal/KDpgvguMpGfqaHPjicRK.svg',
+//   iconfontUrl: '',
+// };
+
+{ "navTheme": "light",
+  "primaryColor": "#1890ff",
+  "layout": "top",
+  "contentWidth": "Fixed",
+  "fixedHeader": true,
+  "fixSiderbar": true,
+  "pwa": false,
+  "logo": "https://gw.alipayobjects.com/zos/rmsportal/KDpgvguMpGfqaHPjicRK.svg",
+  "headerHeight": 48,
+  "splitMenus": false,
+  "footerRender": false,
+  "menuRender": false,
+  "menuHeaderRender": false}
 
 export default Settings;
