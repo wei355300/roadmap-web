5a122fe8edaf468da69769636ac1aa28253805d9
feng.wei <feng.wei@gmail.com>
2022-04-23 14:21:33 +0800

    add roadmap

--- src/pages/RoadMap/service.ts
+++ src/pages/RoadMap/service.ts
@@ -1,6 +1,7 @@
 import request from 'umi-request';
 
 export async function requestDomains(fileName: string): Promise<{ data: string }> {
-  const encodeFileName = Buffer.from(fileName, 'utf-8').toString('base64')
-  return request('/api/git/file/'+encodeFileName, { method: 'GET' });
+  // const encodeFileName = Buffer.from(fileName, 'utf-8').toString('base64')
+  // encodeURI(fileName);
+  return request('/api/git/file/?file='+encodeURI(fileName), { method: 'GET' });
 }
